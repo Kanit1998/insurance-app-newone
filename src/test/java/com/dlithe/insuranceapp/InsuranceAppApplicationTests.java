1029faef70c8f960415434d4e11212719b8452fc
Kanit1998 <102028193+Kanit1998@users.noreply.github.com>
2022-04-26 10:11:41 +0530

    Initial commit

---
+++ src/test/java/com/dlithe/insuranceapp/InsuranceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlithe.insuranceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
